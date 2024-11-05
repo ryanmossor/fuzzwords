3a48c7a097043633c50eb9cc046dcd310f781e07
Ryan Mossor <ryan.mossor@gmail.com>
2024-11-05 17:24:09 -0600

    adds goroutine to ignore additional enter presses while validating answer

--- main.go
+++ main.go
@@ -12,24 +12,43 @@ import (
 )
 
 func main() {
-	cfg := game.InitializeSettings()
-	player := game.InitializePlayer(cfg)
+    cfg := game.InitializeSettings()
+    player := game.InitializePlayer(cfg)
 
-	word_list, err := utils.ReadLines("./wordlist.txt")
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "error: %v\n", err)
-		os.Exit(1)
-	}
+    word_list, err := utils.ReadLines("./wordlist.txt")
+    if err != nil {
+        fmt.Fprintf(os.Stderr, "error: %v\n", err)
+        os.Exit(1)
+    }
 
-	words := game.WordLists{
-		FULL_MAP: utils.ArrToMap(word_list),
-		Available: word_list,
-		Used: make(map[string]bool),
-	}
+    words := game.WordLists{
+        FULL_MAP: utils.ArrToMap(word_list),
+        Available: word_list,
+        Used: make(map[string]bool),
+    }
 
-	reader := bufio.NewReader(os.Stdin)
-	utils.ClearWindow()
+    reader := bufio.NewReader(os.Stdin)
+    input_chan := make(chan string)
+    stop_input := make(chan bool)
+    var ignore_input bool
 
+    // Goroutine to continuously read input
+    go func() {
+        for {
+            select {
+            case <-stop_input: // Stop reading input when receiving from stop_input
+                return
+            default:
+                input, _ := reader.ReadString('\n')
+                if !ignore_input { // Only send to input_chan if ignore_input is false
+                    input_chan <- strings.TrimSpace(input)
+                }
+            }
+        }
+    }()
+
+    utils.ClearWindow()
+	
 	for len(words.Available) > 0 {
 		turn := game.NewTurn(words.Available, cfg)
 
@@ -37,43 +56,48 @@ func main() {
 		fmt.Println("Unused letters:", player.LettersRemaining)
 		fmt.Println()
 
+		turn_loop:
 		for turn.Strikes < cfg.PromptStrikesMax {
 			fmt.Fprintf(os.Stdin, "[ Strikes: %d / %d ]\n", turn.Strikes, cfg.PromptStrikesMax)
+
 			fmt.Println("Prompt:", strings.ToUpper(turn.Prompt))
+			ignore_input = false
 			fmt.Print("Answer: ")
+			answer := <-input_chan
+			ignore_input = true // ignore further Enter presses until next iteration of turn loop
 
-			answer, _ := reader.ReadString('\n')
 			turn.Answer = strings.ToLower(strings.TrimSpace(answer))
-
 			turn.ValidateAnswer(&words, cfg)
 			fmt.Println(turn.Msg)
 
 			if turn.IsValid {
 				player.HandleCorrectAnswer(turn.Answer, &player, cfg)
 				time.Sleep(750 * time.Millisecond)
-				break
+				break turn_loop
 			} else {
 				turn.Strikes += 1
 				fmt.Println()
 			}
-		}
 
-		if cfg.WinCondition == enums.MaxLives && player.HealthCurrent == cfg.HealthMax {
-			fmt.Println("Max lives achieved -- you win!")
-			os.Exit(0)
-		}
-
-		if turn.Strikes == cfg.PromptStrikesMax {
-			fmt.Println("Prompt failed. Possible answer:", turn.SourceWord)
-			player.DecrementHealth()
-
-			if player.HealthCurrent == 0 {
-				fmt.Println()
-				fmt.Println("===== GAME OVER =====")
-				fmt.Println()
+			if cfg.WinCondition == enums.MaxLives && player.HealthCurrent == cfg.HealthMax {
+				fmt.Println("Max lives achieved -- you win!")
+				close(stop_input) // Stop input goroutine on game over
 				os.Exit(0)
-			} else {
-				time.Sleep(2 * time.Second)
+			}
+
+			if turn.Strikes == cfg.PromptStrikesMax {
+				fmt.Println("Prompt failed. Possible answer:", turn.SourceWord)
+				player.DecrementHealth()
+
+				if player.HealthCurrent == 0 {
+					fmt.Println()
+					fmt.Println("===== GAME OVER =====")
+					fmt.Println()
+					close(stop_input)
+					os.Exit(0)
+				} else {
+					time.Sleep(2 * time.Second)
+				}
 			}
 		}
 
@@ -81,5 +105,6 @@ func main() {
 	}
 
 	fmt.Println("Congratulations, you used every word in the dictionary.")
+	close(stop_input)
 	os.Exit(0)
 }
