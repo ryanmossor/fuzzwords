1773e3c855631e7f1069027f33db9e757cf78a79
Ryan Mossor <ryan.mossor@gmail.com>
2026-03-14 23:02:50 -0500

    make manager animations private

--- src/tui/animations/manager.go
+++ src/tui/animations/manager.go
@@ -7,28 +7,26 @@ import (
 )
 
 type AnimationManager struct {
-	// TODO: may want to consider making this map[string][]Animation to register multiple
-	// animations on a single key instead of needing a unique key (target) for each anim
-	Animations map[string]Animation
+	animations map[string]Animation
 }
 
 func InitAnimManager() AnimationManager {
 	return AnimationManager {
-		Animations: make(map[string]Animation),
+		animations: make(map[string]Animation),
 	}
 }
 
 func (m *AnimationManager) Get(key string) (Animation, bool) {
-	anim, ok := m.Animations[key]
+	anim, ok := m.animations[key]
 	return anim, ok
 }
 
 func (m *AnimationManager) Register(key string, val Animation) {
-	m.Animations[key] = val
+	m.animations[key] = val
 }
 
 func (m *AnimationManager) InitAnimations(target_prefix EffectTarget) {
-	for key, anim := range m.Animations {
+	for key, anim := range m.animations {
 		if strings.HasPrefix(key, string(target_prefix)) {
 			slog.Debug("Initializing animation for target",
 				"targetPrefix", target_prefix,
@@ -39,7 +37,7 @@ func (m *AnimationManager) InitAnimations(target_prefix EffectTarget) {
 }
 
 func (m *AnimationManager) Update(now time.Time) {
-	for _, a := range m.Animations {
+	for _, a := range m.animations {
 		a.Update(now)
 	}
 }
@@ -48,7 +46,7 @@ type EffectTarget string
 func (m *AnimationManager) ApplyAnimations(target, text string) (string, bool) {
 	out := text
 	changed := false
-    for key, a := range m.Animations {
+    for key, a := range m.animations {
         if strings.HasPrefix(key, target) && a.IsActive() {
 			slog.Debug("Applying text effect", "target", target, "text", text)
 			out = a.Effect(out)
